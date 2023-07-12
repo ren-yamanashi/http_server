55b7926c0e31504107be929bad423d45b25ad439
ren_yamanashi <ren_yamanashi@mates-system.com>
2023-07-12 22:51:35 +0900

    simplify fro the main.c #22

--- src/server.c
+++ src/server.c
@@ -12,6 +12,8 @@
 #include "io.h"
 
 #define SIZE (5 * 1024)
+#define SERVER_ADDR "127.0.0.1"
+#define SERVER_PORT 8080
 
 /**
  * 受信した文字列を表示
@@ -132,5 +134,73 @@ int httpServer(int sock, Route *routes, int routes_count)
         // NOTE: レスポンスメッセージを送信する
         sendResponseMessage(sock, response_message, response_size);
     }
+    return 0;
+}
+
+int connectHttpServer(Route routes[32], int routes_count)
+{
+    int waiting_sock_addr, connected_sock_addr, DEFAULT_PROTOCOL = 0;
+    struct sockaddr_in sock_addr_info;
+
+    // NOTE: ソケットを作成
+    waiting_sock_addr = socket(AF_INET, SOCK_STREAM, DEFAULT_PROTOCOL);
+    if (waiting_sock_addr == -1)
+    {
+        printf("socket error\n");
+        return -1;
+    }
+
+    // NOTE: 構造体を全て0にセット
+    memset(&sock_addr_info, 0, sizeof(struct sockaddr_in));
+
+    /** サーバーのIPアドレスとボート番号の情報を設定 */
+    // NOTE: アドレスファミリーを指定
+    sock_addr_info.sin_family = AF_INET;
+    // NOTE: 使用するポート番号を指定
+    sock_addr_info.sin_port = htons((unsigned short)SERVER_PORT);
+    // NOTE: 使用するIPアドレスを指定
+    sock_addr_info.sin_addr.s_addr = inet_addr(SERVER_ADDR);
+
+    // NOTE: ソケットを特定のネットワークアドレス（IPアドレスとポート番号の組）に紐付ける
+    if (bind(waiting_sock_addr, (const struct sockaddr *)&sock_addr_info, sizeof(sock_addr_info)) == -1)
+    {
+        printf("bind error\n");
+        close(waiting_sock_addr);
+        return -1;
+    }
+
+    // NOTE: ソケットを接続待ちに設定
+    if (listen(waiting_sock_addr, 3) == -1)
+    {
+        printf("listen error\n");
+        close(waiting_sock_addr);
+        return -1;
+    }
+
+    while (1)
+    {
+        printf("Waiting connect...\n");
+
+        // NOTE: 接続を受け付ける
+        connected_sock_addr = accept(waiting_sock_addr, NULL, NULL);
+        if (connected_sock_addr == -1)
+        {
+            printf("accept error\n");
+            close(waiting_sock_addr);
+            return -1;
+        }
+        printf("Connected!!\n");
+
+        // NOTE: 接続済みのソケットでデータのやり取り
+        httpServer(connected_sock_addr, routes, routes_count);
+
+        // NOTE: ソケット通信をクローズ
+        close(connected_sock_addr);
+
+        // NOTE: 次の接続要求を受け付ける
+    }
+    // NOTE: 接続待ちのソケットをクローズ
+    close(waiting_sock_addr);
+
     return 0;
 }
--- src/server.h
+++ src/server.h
@@ -5,5 +5,6 @@
 
 int httpServer(int sock, Route *routes, int routes_count);
 void showMessage(char *message, unsigned int size);
+int connectHttpServer(Route routes[32], int routes_count);
 
 #endif
