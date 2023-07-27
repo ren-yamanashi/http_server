c32b43db465aa0356a2c7283930fb1bf579b4cee
ren_yamanashi <ren_yamanashi@mates-system.com>
2023-07-28 00:18:22 +0900

    add httpServer node ffi

--- node_ffi_sample/src/index.js
+++ node_ffi_sample/src/index.js
@@ -1,11 +1,75 @@
-const { Library } = require("ffi-napi");
+const { Library, Callback } = require("ffi-napi");
+const ref = require("ref-napi");
+const ArrayType = require("ref-array-napi");
+const StructType = require("ref-struct-napi");
 
-const LIBRARY_FILE = "lib/target/binding";
+/**
+ *
+ * sample ffi
+ *
+ */
+if (process.env.DEV_TYPE == "TEST") {
+  const LIBRARY_FILE = "lib/target/binding";
+  const dll = Library(LIBRARY_FILE, {
+    add: ["int", ["int", "int"]],
+  });
+  const ret = dll.add(1, 2);
+  console.log(ret);
+  return;
+}
 
-const dll = Library(LIBRARY_FILE, {
-  add: ["int", ["int", "int"]],
+/**
+ *
+ * httpServer ffi
+ *
+ */
+const LIBRARY_FILE = "lib/target/libServer";
+const Route = StructType({
+  method: "string",
+  path: "string",
+  content_type: "string",
+  file_path: "string",
+  message: "string",
+  handler: "pointer",
+});
+const RoutePtr = ref.refType(Route);
+const RoutePtrArray = ArrayType(RoutePtr);
+
+const libServer = Library(LIBRARY_FILE, {
+  createRoute: [
+    "void",
+    [RoutePtr, "string", "string", "string", "string", "string", "pointer"],
+  ],
+  runServer: ["int", [RoutePtrArray, "int"]],
 });
 
-const ret = dll.add(1, 2);
+const routes = new Array(2);
+for (let i = 0; i < routes.length; i++) {
+  routes[i] = new Route();
+}
 
-console.log(ret);
+const requestHandler = Callback("void", [], () => {
+  console.log("Request handler called");
+});
+
+libServer.createRoute(
+  routes[0],
+  "GET",
+  "/",
+  "text/html",
+  "/index.html",
+  "",
+  requestHandler
+);
+libServer.createRoute(
+  routes[1],
+  "GET",
+  "/user/:id",
+  "text/html",
+  "/index.html",
+  "",
+  requestHandler
+);
+
+const result = libServer.runServer(routes, routes.length);
+console.log(result);
